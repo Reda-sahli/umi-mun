db57b8ad4f40eeb4cfd66d30d91409727ebb6522
reda sahli <148717510+Reda-sahli@users.noreply.github.com>
2025-05-16 00:22:26 +0100

    Create page.tsx

---
+++ app/pdf/P-UMI-MUN/page.tsx
@@ -0,0 +1,25 @@
+const PUmiMun = () => {
+  return (
+    <div style={{ height: '100vh', width: '100vw', margin: 0, padding: 0 }}>
+      <br />
+      <br /><br />
+      <iframe
+        src="/P-UMI-MUN.pdf"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+          margin: 0,
+          padding: 0,
+          display: 'block',
+        }}
+        title="UMI MUN PDF"
+      >
+        Your browser does not support PDFs. Download it{' '}
+        <a href="/P-UMI-MUN.pdf">here</a>.
+      </iframe>
+    </div>
+  );
+};
+
+export default PUmiMun;
